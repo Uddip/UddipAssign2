21ad636dfe84014cedc36a317090b2b6c5b955ac
Uddip Amin <uddip.amin@gmail.com>
2020-06-20 10:38:30 -0400

    Attempting to instantiate planet array on page load.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@
 * Uddip Amin
 * 991518202
 */
+
+var planetArray = new Array();
+
+$(document).ready(function() {
+  $.getJSON("data/planets.json", function(data) {
+    for (x = 0; x < data.planets.length; x++) {
+      console.log("Plant: " + x);
+      planetArray[x] = data.planets[x];
+    }
+  });
+});

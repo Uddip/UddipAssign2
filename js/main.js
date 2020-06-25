703e85203b1c6188afe428d7231125c1673e6432
Uddip Amin <uddip.amin@gmail.com>
2020-06-24 23:20:53 -0400

    Finished project

--- js/main.js
+++ js/main.js
@@ -3,13 +3,32 @@
 * 991518202
 */
 
-var planetArray = new Array();
-
 $(document).ready(function() {
   $.getJSON("data/planets.json", function(data) {
-    for (x = 0; x < data.solarSystem.planets.length; x++) {
-      console.log("Plant: " + x);
-      planetArray[x] = data.solarSystem.planets[x];
-    }
+    let planetArray = data.solarSystem.planets;
+    $("main").append("<table></table>");
+
+    planetArray.forEach((planet, index) => {
+      $("table").append(`<li>
+                        <div class="card planetCard" style="width: 18rem; border-radius: 25px; border: 1px solid black">
+                        <a
+                          href="uddip.html"
+                          data-index="${index}"
+                          id="${planet.planetName}">
+                          <img src="images/${planet.image}" class="card-img-top" >
+                        </a>
+                          <div class="card-body">
+                            <h5 class="card-title planet_label">${planet.planetName}</h5>
+                          </div>
+                        </div>
+                         </li>`
+                        );
+    });
+
+    $("table a").click(function(){
+      let index = $(this).data("index");
+      localStorage["planet"] = JSON.stringify(planetArray[index]);
+    });
   });
+
 });
--- js/second.js
+++ js/second.js
@@ -2,3 +2,14 @@
 * Uddip Amin
 * 991518202
 */
+
+$(document).ready(function() {
+  let planet = JSON.parse(localStorage["planet"]);
+
+  $("#planet_name").html(planet.planetName);
+  $("#planet_image").attr("src", `images/${planet.image}`);
+  $("#planet_colour").append(planet.planetColor);
+  $("#planet_radius").append(planet.planetRadiusKM + " km");
+  $("#dist_sun").append(planet.distInMillionsKM.fromSun + " million km");
+  $("#dist_earth").append(planet.distInMillionsKM.fromEarth + " million km");
+});
